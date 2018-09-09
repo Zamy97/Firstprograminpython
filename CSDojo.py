13ea44e73aa59eb3dc7e46830757c5fad7bf3829
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-09 00:35:21 -0700

    new function

--- CSDojo.py
+++ CSDojo.py
@@ -2,7 +2,7 @@
 
 def function1():
     print("Hello Fariha")
-    print(" Hello Tasnima")
+    print("Hello Tasnima")
 
 # To call a function you can just use the name of the function to call them. The next line would pring out the values inside it.
 function1()
@@ -13,3 +13,11 @@ def function2(X):
 new_value = function2(8)
 test_value1 = new_value * function2(10)
 print(test_value1)
+
+
+# Function could take more than one argument. For ex:
+
+def function3(x,y):
+    return x*y
+function3_value = function3(8,6)
+print(function3_value)

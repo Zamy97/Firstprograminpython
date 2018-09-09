affb77fb0bdd7d6555198e1f39f73f0d4fbb81d1
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-09 00:28:28 -0700

    added few functions

---
+++ CSDojo.py
@@ -0,0 +1,15 @@
+# Function is collection of instruction that does something.
+
+def function1():
+    print("Hello Fariha")
+    print(" Hello Tasnima")
+
+# To call a function you can just use the name of the function to call them. The next line would pring out the values inside it.
+function1()
+
+# A function could take parameters of argument. Whatever you have inside the parenthasis that is what known as parameter.
+def function2(X):
+    return 9*X
+new_value = function2(8)
+test_value1 = new_value * function2(10)
+print(test_value1)

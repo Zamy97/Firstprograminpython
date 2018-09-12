aeb4777925a880b9b9a2317408333a4674933bf9
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-11 23:36:13 -0700

    added code details

--- challenge-solytion.py
+++ challenge-solytion.py
@@ -1,57 +1,58 @@
-# # I'll print out the solution of the challenges here.
-#
-# #2.3
-#
-# hello_fariha = "Hey, I just wanted to say hi to you. Would you like to learn some Python today."
-# print(hello_fariha)
-#
-# # 2.4
-#
-# first_name = "Fariha"
-# last_name = " Zaman"
-# name = first_name + last_name
-# full_name = "Hello " + " " + name
-#
-# # what this is doing is it's performing a method of making the texts lower case in the variable name name. it's
-# print(full_name.lower())
-#
-# print(full_name.upper())
-#
-# print(full_name.title())
-#
-# # 2.5
-#
-# print(" Sundar Pichai said “Keep pushing your limits.” ")
-#
-# # 2.6
-#
-# famous_person = "Sundar Pichai"
-#
-# famous_person_message = famous_person + " said “Keep pushing your limits.”"
-# print(famous_person_message)
+# I'll print out the solution of the challenges here.
 
-# 2.7
-#
-# Frst_name = " Fariha  "
-# Lst_name = " Zaman  "
-# sundori = Frst_name.lstrip().rstrip() +  Lst_name.lstrip().rstrip()
-# print(sundori.strip())
+#2.3 [  This just prints out the value of the variable in the terminal.]
 
-# # 2.8
-#
-# addition = 5 + 3
-# print(addition)
-#
-# subtraction = 10 - 2
-# print(subtraction)
-#
-# multiplication = 4 * 2
-# print(multiplication)
-#
-# division = round(8 / 1)
-# print(division)
+hello_fariha = "Hey, I just wanted to say hi to you. Would you like to learn some Python today."
+print(hello_fariha)
 
-# 2.9
+# 2.4 [ This was ablout how you can combine two strings together.]
+
+
+first_name = "Fariha"
+last_name = " Zaman"
+name = first_name + last_name
+full_name = "Hello " + " " + name
+
+# what this is doing is it's performing a method of making the texts lower case in the variable name name. it's
+print(full_name.lower())
+
+print(full_name.upper())
+
+print(full_name.title())
+
+# 2.5
+
+print(" Sundar Pichai said “Keep pushing your limits.” ")
+
+# 2.6
+
+famous_person = "Sundar Pichai"
+
+famous_person_message = famous_person + " said “Keep pushing your limits.”"
+print(famous_person_message)
+
+#2.7 [ What this is doing is using strip methods you can get rid of the extra space from the beginning and at the end of the string]
+
+Frst_name = " Fariha  "
+Lst_name = " Zaman  "
+sundori = Frst_name.lstrip().rstrip() +  Lst_name.lstrip().rstrip()
+print(sundori.strip())
+
+# 2.8 [ Good ol additional easy stuff]
+
+addition = 5 + 3
+print(addition)
+
+subtraction = 10 - 2
+print(subtraction)
+
+multiplication = 4 * 2
+print(multiplication)
+
+division = round(8 / 1)
+print(division)
+
+# 2.9 [ What this is doing is showing how you need to convert the value of integer if you want to use it with other strings. You can do it using str() and it'll convert it.]
 
 favorite_number = 19
 fav_msg = "my favorite age number is " + str(favorite_number) + " I don't know why, but I liked being " + str(favorite_number) + " years old."

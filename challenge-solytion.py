2717317d687cf6e970b27e9db7713ea12ef632f9
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-11 19:46:04 -0700

    learning a lot

--- challenge-solytion.py
+++ challenge-solytion.py
@@ -1,1 +1,24 @@
 # I'll print out the solution of the challenges here.
+
+#2.3
+
+hello_fariha = "Hey, I just wanted to say hi to you. Would you like to learn some Python today."
+print(hello_fariha)
+
+# 2.4
+
+first_name = "Fariha"
+last_name = " Zaman"
+name = first_name + last_name
+full_name = "Hello " + " " + name
+
+# what this is doing is it's performing a method of making the texts lower case in the variable name name. it's
+print(full_name.lower())
+
+print(full_name.upper())
+
+print(full_name.title())
+
+# 2.5
+
+print(" Sundar Pichai ")

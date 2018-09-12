115a74ab21e0ed79b76f2386b00098ad3ecf8b75
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-11 22:58:08 -0700

    doing the challengers

--- challenge-solytion.py
+++ challenge-solytion.py
@@ -1,24 +1,38 @@
-# I'll print out the solution of the challenges here.
+# # I'll print out the solution of the challenges here.
+#
+# #2.3
+#
+# hello_fariha = "Hey, I just wanted to say hi to you. Would you like to learn some Python today."
+# print(hello_fariha)
+#
+# # 2.4
+#
+# first_name = "Fariha"
+# last_name = " Zaman"
+# name = first_name + last_name
+# full_name = "Hello " + " " + name
+#
+# # what this is doing is it's performing a method of making the texts lower case in the variable name name. it's
+# print(full_name.lower())
+#
+# print(full_name.upper())
+#
+# print(full_name.title())
+#
+# # 2.5
+#
+# print(" Sundar Pichai said “Keep pushing your limits.” ")
+#
+# # 2.6
+#
+# famous_person = "Sundar Pichai"
+#
+# famous_person_message = famous_person + " said “Keep pushing your limits.”"
+# print(famous_person_message)
 
-#2.3
+# 2.7
 
-hello_fariha = "Hey, I just wanted to say hi to you. Would you like to learn some Python today."
-print(hello_fariha)
-
-# 2.4
-
-first_name = "Fariha"
-last_name = " Zaman"
-name = first_name + last_name
-full_name = "Hello " + " " + name
-
-# what this is doing is it's performing a method of making the texts lower case in the variable name name. it's
-print(full_name.lower())
-
-print(full_name.upper())
-
-print(full_name.title())
-
-# 2.5
-
-print(" Sundar Pichai ")
+Frst_name = " Fariha  "
+Lst_name = " Zaman  "
+sundori = Frst_name.lstrip().rstrip() +  Lst_name.lstrip().rstrip()
+print(sundori.strip())

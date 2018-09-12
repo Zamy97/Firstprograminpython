3b7eb10bd7b35137b531528b6b596b888de81a07
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-11 19:20:27 -0700

    added a few description

--- python-crash-course.py
+++ python-crash-course.py
@@ -1,10 +1,29 @@
-print("hello python interpreter!")
+print("\thello python interpreter!")
 
 simple_message = " The best way yo understand new programming concept is to try using them in your programs."
 
-# print(simple_message)
+print(simple_message)
 
 simple_message = "And you are going to try to apply as much as possible and practice as much as possible to get good at it."
 
+# The (.) after simple_message tells python to perform the title() method on the variable simple_message.
 
 print(simple_message.title())
+
+# The (.) after the simple_message tells pythong to perform the upper() method on the variable simple_message. What .upper() is doing is it's telling python to make all the values of simple_message uppercase.
+
+print(simple_message.upper())
+
+# What .lower() method is doing here is it's telling python to make everything lowercase in the variable simple_message by performing that action.
+
+print(simple_message.lower())
+
+
+iphone = '     macs are really really expensive      '
+
+# What this is doing is it's telling the lstrip and rstrip to act on the iphone varible. What it'll do is it'll get rid of the space from both sides of the value of the variable.
+
+Macs = iphone.rstrip().lstrip()
+
+
+print(Macs.strip())

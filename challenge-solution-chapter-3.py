9ae98465617373871b905d591ce247493c4e3ba6
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-13 00:31:12 -0700

    doing 3.7

--- challenge-solution-chapter-3.py
+++ challenge-solution-chapter-3.py
@@ -24,23 +24,42 @@
 
 # 3.4
 
-my_guests = ['Michael Sayman', 'Tim Cook', 'Anthony D. Mays', 'Sundar Pichai', 'shuo-from-intel' ]
+# my_guests = ['Michael Sayman', 'Tim Cook', 'Anthony D. Mays', 'Sundar Pichai', 'shuo-from-intel' ]
+#
+#
+# message_to_guest0 = "Hello " + my_guests[0] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
+#
+# message_to_guest1 = "Hello " + my_guests[1] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
+#
+# message_to_guest2 = "Hello " + my_guests[2] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
+#
+#
+#
+# print(message_to_guest0.title())
+# print(message_to_guest1.title())
+# print(message_to_guest2.title())
+#
+#
+# # 3.5
+#
+# my_guests = ['Michael Sayman', 'Tim Cook', 'Anthony D. Mays', 'Sundar Pichai', 'shuo-from-intel' ]
+#
+# my_guests.pop(0)
+# my_guests.insert(0, 'Fariha')
+#
+# message_to_guest0 = "Hello " + my_guests[0] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
+#
+# message_to_guest1 = "Hello " + my_guests[1] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
+#
+# message_to_guest2 = "Hello " + my_guests[2] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
+#
+#
+#
+# print(message_to_guest0.title())
+# print(message_to_guest1.title())
+# print(message_to_guest2.title())
 
-
-message_to_guest0 = "Hello " + my_guests[0] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
-
-message_to_guest1 = "Hello " + my_guests[1] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
-
-message_to_guest2 = "Hello " + my_guests[2] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
-
-
-
-print(message_to_guest0.title())
-print(message_to_guest1.title())
-print(message_to_guest2.title())
-
-
-# 3.5
+# 3.6
 
 my_guests = ['Michael Sayman', 'Tim Cook', 'Anthony D. Mays', 'Sundar Pichai', 'shuo-from-intel' ]
 
@@ -49,12 +68,4 @@ my_guests.insert(0, 'Fariha')
 
 message_to_guest0 = "Hello " + my_guests[0] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
 
-message_to_guest1 = "Hello " + my_guests[1] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
-
-message_to_guest2 = "Hello " + my_guests[2] + "I am inviting you to have a dinner with me so I'd get to know you a bit more and may be a get a bit of advice from you"
-
-
-
 print(message_to_guest0.title())
-print(message_to_guest1.title())
-print(message_to_guest2.title())

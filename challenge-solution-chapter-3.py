73d922300e023e9a2f1abf675b7af376d7d51a70
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-12 22:34:54 -0700

    solving problems

--- challenge-solution-chapter-3.py
+++ challenge-solution-chapter-3.py
@@ -1,15 +1,23 @@
 # Chapter 3
 
 # 3.1
+#
+# names = ['Fariha', 'Andrew', 'Karen', 'Gelle', 'Mahdi']
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[4])
+# print(names[-4])
+#
+# # 3.2
+#
+# message_for_names = "Hey " + names[0] + " I just wanted to tell you that you are beautiful"
+# print(message_for_names.title())
+#
 
-names = ['Fariha', 'Andrew', 'Karen', 'Gelle', 'Mahdi']
-print(names[0])
-print(names[1])
-print(names[2])
-print(names[3])
-print(names[4])
+# 3.3
 
-# 3.2
-
-message_for_names = "Hey " + names[0] + " I just wanted to tell you that you are beautiful"
-print(message_for_names)
+my_fav_transportation = ['Tesla', 'Train', 'Bus', 'plane', 'Bicycle']
+message_for_transportation = " One day I am going to buy a " + my_fav_transportation[0] + " and I'll cruise around with my Mom and give her a taste of what it feels like to be in an expensive car( Inshallah)"
+print(message_for_transportation.lstrip().title())

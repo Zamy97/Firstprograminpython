39361982e6d538e0cb9bd4fefdef47214ade1c75
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-27 23:47:31 -0700

    testing

--- Chapter-5.py
+++ Chapter-5.py
@@ -14,4 +14,10 @@ requested_topping = 'mushroom'
 if requested_topping != 'pencil':
     print("hold the anchovies")
 
-# Numerical Comparism
+# Checking Whether a Value Is Not in a List
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'andrew'
+
+if user not in banned_users:
+    print(user.title() + ", you can do whatever you want")

b1a1b54962f23faa7d8dc6cfc00fada1aaf64829
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-27 23:28:51 -0700

    numerical comparison

--- Chapter-5.py
+++ Chapter-5.py
@@ -1,7 +1,6 @@
 # A Simple Example of IF statememnt
 
 # cars = ['audi', 'bmw', 'subaru', 'toyota']
-#
 # for car in cars:
 #     if car == 'bmw':
 #         print(car.upper())
@@ -10,6 +9,9 @@
 
 
 # Checking for inequality
+
 requested_topping = 'mushroom'
 if requested_topping != 'pencil':
     print("hold the anchovies")
+
+# Numerical Comparism

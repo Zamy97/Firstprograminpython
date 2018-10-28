11ccbdb709583f89bbe2f13bcbee00da30d9df22
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-28 01:05:25 -0700

    on chapter 5

--- Chapter-5.py
+++ Chapter-5.py
@@ -24,21 +24,34 @@
 
 # 5.1 (Conditional Test)
 
-# car = 'subaru'
-# print("Is car == 'subaru' ?? I predict subaru")
-# print(car == 'subaru')
-#
-# animal = "monkey"
-# print("Guess the animal? Is it monkey?")
-# print(animal == "lion")
-#
-# is_passed = "passed"
-# print("Do you think you passed the test? type Passed or failed?")
-# print(is_passed == "passed")
+car = 'subaru'
+print("Is car == 'subaru' ?? I predict subaru")
+print(car == 'subaru')
+
+animal = "monkey"
+print("Guess the animal? Is it monkey?")
+print(animal == "lion")
+
+is_passed = "passed"
+print("Do you think you passed the test? type Passed or failed?")
+print(is_passed == "passed")
 
 #Just playing with the if statement
-hungry_input = input("Are you hungry? Yes or no?: ")
-if hungry_input == 'yes':
-    print("You need to go eat then.")
-else:
-    print("well then I am sorry for you!")
+# hungry_input = input("Are you hungry? Yes or no?: ")
+# if hungry_input == 'yes':
+#     print("You need to go eat then.")
+# else:
+#     print("well then I am sorry for you!")
+
+thirsty = "yes"
+print("Are you thirsty? Want some water")
+print(thirsty == "no")
+
+
+own_a_car = "hell ya"
+print("Do you have your own car??")
+print(own_a_car == "hell ya")
+
+
+# 5.2 (More conditional tests)
+#Start from page 82 next time

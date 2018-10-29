64ecb5338a1dc278ccdd36e20200e5f50d8dd416
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-28 20:06:33 -0700

    learning about if-elif-else chain

--- Chapter-5.py
+++ Chapter-5.py
@@ -24,17 +24,17 @@
 
 # 5.1 (Conditional Test)
 
-car = 'subaru'
-print("Is car == 'subaru' ?? I predict subaru")
-print(car == 'subaru')
-
-animal = "monkey"
-print("Guess the animal? Is it monkey?")
-print(animal == "lion")
-
-is_passed = "passed"
-print("Do you think you passed the test? type Passed or failed?")
-print(is_passed == "passed")
+# car = 'subaru'
+# print("Is car == 'subaru' ?? I predict subaru")
+# print(car == 'subaru')
+#
+# animal = "monkey"
+# print("Guess the animal? Is it monkey?")
+# print(animal == "lion")
+#
+# is_passed = "passed"
+# print("Do you think you passed the test? type Passed or failed?")
+# print(is_passed == "passed")
 
 #Just playing with the if statement
 # hungry_input = input("Are you hungry? Yes or no?: ")
@@ -43,15 +43,44 @@ print(is_passed == "passed")
 # else:
 #     print("well then I am sorry for you!")
 
-thirsty = "yes"
-print("Are you thirsty? Want some water")
-print(thirsty == "no")
-
-
-own_a_car = "hell ya"
-print("Do you have your own car??")
-print(own_a_car == "hell ya")
+# thirsty = "yes"
+# print("Are you thirsty? Want some water")
+# print(thirsty == "no")
+#
+#
+# own_a_car = "hell ya"
+# print("Do you have your own car??")
+# print(own_a_car == "hell ya")
 
 
 # 5.2 (More conditional tests)
-#Start from page 82 next time
+# No that important! just do something and move on!
+
+# IF STATEMENT
+
+# age = 12
+#
+# if age >= 18:
+#     print("you are old enough to vote, now go select a good president for our future")
+# else:
+#     print("sorry you are too young to vote")
+
+
+
+# Amusement park ticket test
+
+age = 67
+
+if age < 4:
+    price = 0
+
+elif age < 18:
+    price = 5
+
+elif age < 65:
+    price = 10
+
+else:
+    price = 5
+
+print("Your Admission ticket is $" + str(price) + ".")

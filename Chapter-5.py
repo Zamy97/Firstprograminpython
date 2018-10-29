e06fc6161677aaccacad80d1fe0ae8f9d7392260
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-28 21:22:46 -0700

    on exercise 5.6

--- Chapter-5.py
+++ Chapter-5.py
@@ -69,18 +69,101 @@
 
 # Amusement park ticket test
 
-age = 67
+# age = 67
+#
+# if age < 4:
+#     price = 0
+#
+# elif age < 18:
+#     price = 5
+#
+# elif age < 65:
+#     price = 10
+#
+# else:
+#     price = 5
+#
+# print(" \nYour Admission ticket is $" + str(price) + ".")
 
-if age < 4:
-    price = 0
 
-elif age < 18:
-    price = 5
 
-elif age < 65:
-    price = 10
 
-else:
-    price = 5
+# Pizzaria example
 
-print("Your Admission ticket is $" + str(price) + ".")
+# requested_topping = ['mushrooms', 'extra cheese']
+#
+# if 'mushrooms' in requested_topping:
+#     print("Don't forget to add mushrooms on the pizza")
+# if 'pepperoni' in requested_topping:
+#     print("Don't forget on add pepperoni")
+# if 'extra cheese' in requested_topping:
+#     print("I'll make sure to add some extra cheese for you")
+#
+# print("\nFinished making your pizza!")
+
+
+
+# 5.3 (Alien Colors-1)
+
+# alien_color = "Red"
+# if alien_color is "Green":
+#     print("You earned 5 points")
+# elif alien_color is "Red":
+#     print("You get 5 points.")
+# elif alien_color is "Brown":
+#     print("you get 5 points")
+
+# Version that doesn't pass the condition at all!
+
+# different_alien_color = "Brown"
+# if different_alien_color is "Pink":
+#     print("You get 5 points")
+# elif different_alien_color is "Blacl":
+#     print(" You get 5 points")
+# elif different_alien_color is "White":
+#     print("You get 5 points")
+
+
+# 5.4 (Alien Color-2)
+
+# alien_colors = "Green"
+#
+# if alien_colors is "Blue":
+#     print("you just earned 5 points for shooting the alien")
+# else:
+#     print("you just earned 10 points for nothing lol")
+
+
+# 5.5 (Alien Color-3 using if-elif-else)
+
+first_alien_color = "Blue"
+
+if first_alien_color is "Green":
+    print("Good job! You just earned 5 points")
+elif first_alien_color is "Blue":
+    print("You just earned 10 points")
+elif first_alien_color is "Red":
+    print("you just earned 15 points")
+
+
+second_alien_color = "Green"
+
+if second_alien_color is "Green":
+    print("you just earned 5 points")
+elif second_alien_color is "Red":
+    print("You just earned 15 points")
+elif second_alien_color is "Blue":
+    pritn("you just earned 10 points")
+
+
+third_alien_color = "Red"
+
+if third_alien_color is "Red":
+    print("you just earned 15 points")
+elif third_alien_color is "Green":
+    print("you don't get anything")
+elif third_alien_color is "Blue":
+    print("just go home man")
+
+
+# 5.6 (Stages of life)

b7d6ad624cdda18252161b42830ec0c0b5ebc3a1
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-01 16:30:09 -0700

    learning about function

---
+++ Chapter-8.py
@@ -0,0 +1,20 @@
+# A Simple function
+def greet_user(username):
+    """Display a simple greeeting."""
+    print("Hello "+ username.title()+ "!")
+
+greet_user("Aktar")
+
+
+# 8.1 (Message)
+def display_message():
+    print("I am learning about functions in this chapter because I am not comfortable with how to write them so I want to learn it in this chapter and be good at it! Uk wt m sayinn")
+
+display_message()
+
+# 8.2 (Favorite book)
+
+def favorite_book(title):
+    print("One of my favorite book is :" + title.title())
+
+favorite_book(" If Beale Street could talk")

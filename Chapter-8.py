1e37cc66b7d9c3e83efad7b56f1ff70e7abbb2e9
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-01 19:22:49 -0700

    getting good at function

--- Chapter-8.py
+++ Chapter-8.py
@@ -79,15 +79,44 @@
 
 # Making an argument optional
 
-def get_full_name(first_name, last_name, middle_name=""):
-    '''Return full name clearly"'''
-    if middle_name:
-        full_name = first_name + " " + middle_name + " " +last_name
-    else:
-        full_name = first_name + " " + last_name
-    return full_name.title()
+# def get_full_name(first_name, last_name, middle_name=""):
+#     '''Return full name clearly"'''
+#     if middle_name:
+#         full_name = first_name + " " + middle_name + " " +last_name
+#     else:
+#         full_name = first_name + " " + last_name
+#     return full_name.title()
+#
+# my_full_name = get_full_name("Aktar","Zaman")
+# print(my_full_name)
+# my_full_name = get_full_name("Aktar","Zaman","Uz")
+# print(my_full_name)
 
-my_full_name = get_full_name("Aktar","Zaman")
-print(my_full_name)
-my_full_name = get_full_name("Aktar","Zaman","Uz")
-print(my_full_name)
+# Build a person
+
+# def build_person(first_name,last_name):
+#     '''return a dictionary of information about a person'''
+#     person = {
+#         'first':first_name,
+#         'last':last_name
+#         }
+#     return person
+#
+# full_name = build_person("Fariha","Zaman")
+# print(full_name)
+
+# making a person and their details
+
+def person_details(first_name,last_name,age=""):
+
+    person = {
+        'first':first_name,
+        'last':last_name
+        }
+    if age:
+        person['age'] = age
+    return person
+persons_name = person_details("Aktar","Zaman",21)
+print(persons_name)
+
+# Start from Using a Function with a while Loop on page - 145

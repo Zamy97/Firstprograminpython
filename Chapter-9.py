de3f98c6bc909a40bac6c5e850214c628ccdef70
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-03 00:10:52 -0800

    done with few exercises

--- Chapter-9.py
+++ Chapter-9.py
@@ -56,3 +56,23 @@ second_instance = Restaurant("In n out", "Fast ")
 second_instance.describe_reasturant()
 
 third_instance = Restaurant("Somethign", "something")
+third_instance.describe_reasturant()
+
+# 9.3(Users)
+
+class User():
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def describe_user(self):
+        """ Details about the user"""
+        print("My first name is " + self.first_name + " and My last name is " + self.last_name)
+
+    def greet_user(self):
+        print("Hello Beautiful " + self.first_name + " "+  self.last_name)
+
+user1 = User("Aktar", "Zaman")
+user1.describe_user()
+user1.greet_user()

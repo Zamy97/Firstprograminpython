9f2a7d897f36c270374b618d7d51785628444d40
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-03 18:54:26 -0800

    on page 176

--- Chapter-9.py
+++ Chapter-9.py
@@ -79,42 +79,42 @@
 
 # Car class
 
-# class Car():
-#
-#     def __init__(self, make, model, year):
-#         self.make = make
-#         self.model= model
-#         self.year = year
-#         self.odometer_reading = 0
-#
-#     def get_desctiptive_name(self):
-#         """Return a neatly formatted descriptive name"""
-#         long_name = str(self.year) + ' ' + self.make + ' '+self.model
-#         return long_name.title()
-#
-#     def read_odometer(self):
-#         """print a statement showing the car's milege"""
-#         print("This car has " +str(self.odometer_reading) + " miles on it.")
-#
-#     def update_odometer(self, mileage):
-#         """
-#         set the odometer reading to given valueself.
-#
-#         Reject the change if it attempts to roll the odometer back.
-#
-#         """
-#
-#         if mileage >= self.odometer_reading:
-#             self.odometer_reading = mileage
-#         else:
-#             print("Cmo'n man! Y u trying to lie to me")
-#
-#     def increment_odometer(self, miles):
-#         """add the given amount to the odometer"""
-#         if miles >= self.odometer_reading:
-#             self.odometer_reading += miles
-#         else:
-#             print("Get a life")
+class Car():
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model= model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_desctiptive_name(self):
+        """Return a neatly formatted descriptive name"""
+        long_name = str(self.year) + ' ' + self.make + ' '+self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        """print a statement showing the car's milege"""
+        print("This car has " +str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        set the odometer reading to given valueself.
+
+        Reject the change if it attempts to roll the odometer back.
+
+        """
+
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("Cmo'n man! Y u trying to lie to me")
+
+    def increment_odometer(self, miles):
+        """add the given amount to the odometer"""
+        if miles >= self.odometer_reading:
+            self.odometer_reading += miles
+        else:
+            print("Get a life")
 
 # my_new_car = Car('Scion', 'FRS', 2017)
 # print(my_new_car.get_desctiptive_name())
@@ -207,3 +207,34 @@ user1.increment_login_attempts()
 # user1.reset_login_attemps()
 
 print(user1.login_attempts)
+
+# Battery class
+
+class Battery():
+
+    def __init__(self, battery_size = 70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles"""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class"""
+        super().__init__(make, model, year)
+        self.battery = Battery(600)
+
+    # def describe_battery(self):
+    #     """Print a statement describing the battery size."""
+    #     print(self.make +" "+ self.model.title() + " has a " + str(self.battery_size) + "-kWh battery.")
+
+
+my_tesla = ElectricCar("Tesla", "model s", 2016)
+print(my_tesla.get_desctiptive_name())
+my_tesla.battery.describe_battery()
+
+# Start from page 176

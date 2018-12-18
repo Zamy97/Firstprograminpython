a7da305e778bfe12d1a8c9859a872c14c4c01710
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-17 20:12:40 -0800

    learning about exception

--- Chapter-10.py
+++ Chapter-10.py
@@ -97,3 +97,19 @@ while True:
         with open(filename, 'a') as f:
             f.write(name + "\n")
         print("Hi " + name + ", you've been added to the guest book.")
+
+## 10.5 (Programming Poll)
+filename = 'programming_poll.txt'
+
+responses = []
+while True:
+    response = input("\nWhy do you like programming? ")
+    responses.append(response)
+
+    continue_poll = input("Would you like to let someone else respond? (y/n) ")
+    if continue_poll != 'y':
+        break
+
+with open(filename, 'a') as f:
+    for response in responses:
+        f.write(response + "\n")

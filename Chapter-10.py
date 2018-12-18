bf3cbc5b6a97891ad04f1c1ded07faf76ab6a0c1
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-17 18:50:35 -0800

    working on reading from file

--- Chapter-10.py
+++ Chapter-10.py
@@ -1,5 +1,14 @@
 ## reading from a file
 
-with open('pi_digits.txt')as file_object:
+with open('text_files/pi_digits.txt')as file_object:
     contents = file_object.read()
-    print(contents)
+    print(contents.strip())
+
+## Read a file from a specific location
+
+# with open('Specific file route')
+
+## Absoulate path
+
+# Absolute paths are usually longer than relative paths, so it’s helpful to store them in a variable and then pass that variable to open(). On Linux and OS X, absolute paths look like this:
+# file_path = '/home/ehmatthes/other_files/text_files/filename.txt' with open(file_path) as file_object:

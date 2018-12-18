774f15885d1403be432d598e1c1dce51bf37daf3
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-17 19:16:13 -0800

    working with files

--- Chapter-10.py
+++ Chapter-10.py
@@ -36,5 +36,11 @@ pi_string = ''
 for line  in lines:
     pi_string += line.strip()
 
-print(pi_string[:9])
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the few digits of pi that I have collected!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
+
+print(pi_string[:])
 print(len(pi_string))

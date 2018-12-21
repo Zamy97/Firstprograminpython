d71e6f2285f5e035d6e0a12d9de989bdff314831
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-20 20:27:57 -0800

    onto random walks

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 x_values = list(range(1,1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
+
+# plt.scatter(x_values, y_values, c='red', edgecolor="none", s=40)
+
 # plt.scatter(3, 5, s=200)
 #
 # # Set chart title and label axes.

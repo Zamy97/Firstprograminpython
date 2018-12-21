48b29f91e852195677e8d83110a55437888175b4
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-21 00:03:08 -0800

    working on plotting points

--- rw_viaual.py
+++ rw_viaual.py
@@ -6,7 +6,7 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
@@ -15,6 +15,10 @@ while True:
     # Emphasize the first and the last points
     plt.scatter(0,0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red',edgecolors='none',s=100)
+
+    # Remove the axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = input("Make Another walk? (y/n): ")
